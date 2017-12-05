322ab2e2e094d4b3ba319b9e9c994ac67f02da95
Jeho <jeho328@naver.com>
2017-12-05 22:05:23 +0900

    Setting Up a Development Environment

--- app/src/main/java/com/example/cjnote/ae4teamapp/LoginActivity.java
+++ app/src/main/java/com/example/cjnote/ae4teamapp/LoginActivity.java
@@ -1,13 +1,260 @@
 package com.example.cjnote.ae4teamapp;
 
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 
-public class LoginActivity extends AppCompatActivity {
+import org.w3c.dom.Text;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LoginActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
+    private FirebaseAuth mAuth;
+    private GoogleApiClient mGoogleApiClient;
+    private static final String TAG = "Google SignIn Firebase";
+    private static final int RC_SIGN_IN = 123;
+    private CollectionReference userColRef = FirebaseFirestore.getInstance().collection("user");
+    private ProgressDialog dialog;
+    private Boolean status = false;
+    private final long FINISH_INTERVAL_TIME = 2000;
+    private long   backPressedTime = 0;
+    final Context context = this;
+    private TextView nicknameTextView;
+    @Override
+    public void onBackPressed() {
+        long tempTime = System.currentTimeMillis();
+        long intervalTime = tempTime - backPressedTime;
+
+        if (0 <= intervalTime && FINISH_INTERVAL_TIME >= intervalTime)
+        {
+            super.onBackPressed();
+        }
+        else
+        {
+            backPressedTime = tempTime;
+            Toast.makeText(getApplicationContext(), "뒤로가기 버튼을 한번더 누르면 종료됩니다.", Toast.LENGTH_SHORT).show();
+        }
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        mAuth = FirebaseAuth.getInstance();
+        dialog = new ProgressDialog(LoginActivity.this);
+        dialog.setMessage("로딩중..");
+        dialog.setCancelable(false);
+        nicknameTextView = (TextView)findViewById(R.id.userNickName);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this /* FragmentActivity */, this /* OnConnectionFailedListener */)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
+        findViewById(R.id.googleButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mAuth.getCurrentUser() == null) {
+                    status = true;
+                    dialog.show();
+                }
+                signIn();
+            }
+        });
+
+        findViewById(R.id.signOutButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mAuth.getCurrentUser() != null) {
+                    signOut();
+                }
+            }
+        });
+
+        findViewById(R.id.nonMemberButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mAuth.getCurrentUser() == null) {
+                    selectActivity();
+                }
+            }
+        });
+
+        if (mAuth.getCurrentUser() != null) {
+            nicknameTextView.setText(mAuth.getCurrentUser().getDisplayName()+"님 환영합니다");
+        }
+    }
+
+    private void signIn() {
+        if (mAuth.getCurrentUser() != null) {
+            selectActivity();
+            return;
+        }
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    private void signOut() {
+        mAuth.signOut();
+        nicknameTextView.setText("no user");
+        Toast.makeText(LoginActivity.this, "로그아웃 되었습니다", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            if (result.isSuccess()) {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = result.getSignInAccount();
+                firebaseAuthWithGoogle(account);
+            } else {
+                // Google Sign In failed, update UI appropriately
+                // ...
+            }
+        }
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+        Log.d(TAG, "firebaseAuthWithGoogle:" + acct.getId());
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            FirebaseUser user = mAuth.getCurrentUser();
+
+                            Map<String, Object> userMap = new HashMap<>();
+
+                            userMap.put("email", user.getEmail());
+                            userMap.put("nickname", user.getDisplayName());
+                            userColRef.document(user.getUid())
+                                    .set(userMap)
+                                    .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                        @Override
+                                        public void onSuccess(Void aVoid) {
+                                            Log.d(TAG, "로그인 되었습니다.");
+                                            nicknameTextView.setText(mAuth.getCurrentUser().getDisplayName()+"님 환영합니다");
+                                            selectActivity();
+                                        }
+                                    })
+                                    .addOnFailureListener(new OnFailureListener() {
+                                        @Override
+                                        public void onFailure(@NonNull Exception e) {
+                                            Log.w(TAG, "Error writing document", e);
+                                            Toast.makeText(LoginActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                                            mAuth.getCurrentUser().delete()
+                                                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                        @Override
+                                                        public void onComplete(@NonNull Task<Void> task) {
+                                                            if (task.isSuccessful()) {
+                                                                Log.d(TAG, "User account deleted.");
+                                                            }
+                                                        }
+                                                    });
+                                        }
+                                    })
+                                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<Void> task) {
+                                            dialog.dismiss();
+                                        }
+                                    });
+                        } else {
+                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+        dialog.dismiss();
+    }
+
+    public void selectActivity() {
+        if(status) {
+            Toast.makeText(LoginActivity.this, "로그인되었습니다 " + mAuth.getCurrentUser().getDisplayName() + "님", Toast.LENGTH_LONG).show();
+            status = false;
+        }
+
+        final CharSequence[] activities = {"Test1", "Test2", "Test3"};
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(context);
+
+        // 다이얼로그 셋팅
+        alertDialogBuilder.setTitle("액티비티를 선택하세요");
+        alertDialogBuilder.setItems(activities, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int id) {
+                Toast.makeText(getApplicationContext(),
+                        activities[id] + " 선택했습니다.",
+                        Toast.LENGTH_SHORT).show();
+                dialog.dismiss();
+
+                // 액티비티 이동
+                moveActivity(id);
+            }
+        });
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        alertDialog.show();
+    }
+
+    public void moveActivity(int id) {
+        switch (id) {
+            case 0:
+                Intent test1Intent = new Intent(LoginActivity.this,  Test1Activity.class);
+                startActivity(test1Intent);
+                break;
+            case 1:
+                Intent test2Intent = new Intent(LoginActivity.this,  Test2Activity.class);
+                startActivity(test2Intent);
+                break;
+            case 2:
+                Intent test3Intent = new Intent(LoginActivity.this,  Test3Activity.class);
+                startActivity(test3Intent);
+                break;
+        }
     }
 }
---
+++ app/src/main/java/com/example/cjnote/ae4teamapp/Test3Activity.java
@@ -0,0 +1,13 @@
+package com.example.cjnote.ae4teamapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Test3Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test3);
+    }
+}
